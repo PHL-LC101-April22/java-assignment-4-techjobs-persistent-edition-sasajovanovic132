0e49f1fff3d410595aa6af9998c6f2a384f544b0
sasajovanovic132 <75175528+sasajovanovic132@users.noreply.github.com>
2022-10-05 02:05:07 -0400

    Task 1 and 2 passed

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @Size(min=10,max=200)
+private String description;
+    public Skill(){}
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }

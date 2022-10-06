a6474090b86dda49b311f934b31a460dfc358599
sasajovanovic132 <75175528+sasajovanovic132@users.noreply.github.com>
2022-10-06 00:38:09 -0400

    Task three passed

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,13 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
     @NotBlank(message = "Location is required!")
     @Size(min=3,max=50,message="Location must be between 3 - 50 characters long")
     private String location;
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
    public Employer(){}
 
     public String getLocation() {
